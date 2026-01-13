27a5767955ed7193bc29dd52b84949d94bc36db9
ksmit <korie.smith@bsu.edu>
2026-01-13 13:31:41 -0500

    Remove unnecessary vertical space

--- test/leap_year_calculator_test.dart
+++ test/leap_year_calculator_test.dart
@@ -1,10 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:test1/leap_year_calculator.dart';
 
-
 void main () {
   final calculator = LeapYearCalculator();
-
   final map = {
     2024: true,
     2026: false,
@@ -20,37 +18,6 @@ void main () {
       expect(result, actual);
     });
   }
-
-//  test('2024 is a leap year', () {
-//    final result = calculator.isLeapYear(2024);
-//    expect(true, result);
-//  });
-//
-//
-//  test('2026 is not a leap year', () {
-//    final result = calculator.isLeapYear(2026);
-//    expect(false, result);
-//  });
-//
-//
-//  test('1900 is not a leap year', () {
-//    final result = calculator.isLeapYear(1900);
-//    expect(false, result);
-//  });
-//
-//
-//  test('1 is not a leap year', () {
-//    final result = calculator.isLeapYear(1);
-//    expect(false, result);
-//  });
-//
-//
-//  test('400 is a leap year', () {
-//    final result = calculator.isLeapYear(400);
-//    expect(true, result);
-//  });
-//
-//
 }
 
 
