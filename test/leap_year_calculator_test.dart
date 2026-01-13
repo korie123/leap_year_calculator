fa286dc14e147b44311f41395056c555847b9211
ksmit <korie.smith@bsu.edu>
2026-01-13 13:30:12 -0500

    Change

--- test/leap_year_calculator_test.dart
+++ test/leap_year_calculator_test.dart
@@ -3,41 +3,54 @@ import 'package:test1/leap_year_calculator.dart';
 
 
 void main () {
-  test('2024 is a leap year', () {
-    final calculator = LeapYearCalculator();
-    final result = calculator.isLeapYear(2024);
-    expect(true, result);
-  });
+  final calculator = LeapYearCalculator();
 
+  final map = {
+    2024: true,
+    2026: false,
+    1900: false,
+    1: false,
+    400: true,
+  };
 
-  test('2026 is not a leap year', () {
-    final calculator = LeapYearCalculator();
-    final result = calculator.isLeapYear(2026);
-    expect(false, result);
-  });
+  for (int year in map.keys) {
+    final result = map[year];
+    test('$year $result', () {
+      final actual = calculator.isLeapYear(year);
+      expect(result, actual);
+    });
+  }
 
-
-  test('1900 is not a leap year', () {
-    final calculator = LeapYearCalculator();
-    final result = calculator.isLeapYear(1900);
-    expect(false, result);
-  });
-
-
-  test('1 is not a leap year', () {
-    final calculator = LeapYearCalculator();
-    final result = calculator.isLeapYear(1);
-    expect(false, result);
-  });
-
-
-  test('400 is a leap year', () {
-    final calculator = LeapYearCalculator();
-    final result = calculator.isLeapYear(400);
-    expect(true, result);
-  });
-
-
+//  test('2024 is a leap year', () {
+//    final result = calculator.isLeapYear(2024);
+//    expect(true, result);
+//  });
+//
+//
+//  test('2026 is not a leap year', () {
+//    final result = calculator.isLeapYear(2026);
+//    expect(false, result);
+//  });
+//
+//
+//  test('1900 is not a leap year', () {
+//    final result = calculator.isLeapYear(1900);
+//    expect(false, result);
+//  });
+//
+//
+//  test('1 is not a leap year', () {
+//    final result = calculator.isLeapYear(1);
+//    expect(false, result);
+//  });
+//
+//
+//  test('400 is a leap year', () {
+//    final result = calculator.isLeapYear(400);
+//    expect(true, result);
+//  });
+//
+//
 }
 
 
